5e93a49f2397f54e187fbacaf57b661e53fad378
Victory Chianumba <chianumbav@gmail.com>
2025-06-26 21:43:28 +0100

    looking at data, sorting the EEG net features

--- models/eegnet.py
+++ models/eegnet.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 class EEGNet(nn.Module):
     def __init__(self, num_classes=4, channels=22, samples=1125):
         super(EEGNet, self).__init__()
+        
+        self.activations = {}
 
         self.firstconv = nn.Sequential(
             nn.Conv2d(1, 16, (1, 51), padding=(0, 25), bias=False),
@@ -31,7 +33,11 @@ class EEGNet(nn.Module):
 
     def forward(self, x):
         x = self.firstconv(x)
+        self.activations['firstconv'] = x.detach().cpu()
         x = self.depthwiseConv(x)
+        self.activations['depthwiseConv'] = x.detach().cpu()
         x = self.separableConv(x)
+        self.activations['separableConv'] = x.detach().cpu()
+
         x = x.view(x.size(0), -1)  # flatten
         return self.classify(x)
