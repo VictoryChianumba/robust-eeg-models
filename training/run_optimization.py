b8cbfa6176f81865239a96775450bb88ec6444d3
Victory Chianumba <chianumbav@gmail.com>
2025-07-08 15:31:17 +0000

    training optimisation files

--- training/run_optimization.py
+++ training/run_optimization.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from training.create_and_train_model import create_and_train_model
 
-def run_optimization(X, y, model_type='EEGNet', n_trials=50):
+def run_optimization(X, y, model_type=model_type, n_trials=n_trials):
     # Data preparation (done once)
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
